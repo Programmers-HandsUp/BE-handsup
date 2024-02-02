f7fcf4802cfc35fd41dbcd78b9a8f369af8bb907
hs12 <loveanna1228@gmail.com>
2024-02-03 00:52:28 +0900

    [init] : 프로젝트 생성

---
+++ src/main/java/com/example/handsup/HandsUpApplication.java
@@ -0,0 +1,13 @@
+package com.example.handsup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HandsUpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HandsUpApplication.class, args);
+    }
+
+}
