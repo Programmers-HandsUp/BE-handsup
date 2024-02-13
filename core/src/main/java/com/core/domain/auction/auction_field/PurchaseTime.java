0d280f6dab7886999a47b7d3778593856c9060bd
Son Gahyun <77109954+hyun2371@users.noreply.github.com>
2024-02-14 03:17:30 +0900

    [feat] : 엔티티 세팅 (#15)
    * [feat] : base entity 추가
    
    * [feat] : auction 관련 entity 추가
    
    * [feat] : chat 관련 엔티티 추가
    
    * [feat] : 알림 관련 엔티티 추가
    
    * [feat] 후기 관련 엔티티 추가
    
    * [feat] 유저 관련 엔티티 추가
    
    * [refactor] 유저 아이디 필드명 수정
    
    * [refactor] : 유저 필드명 수정

---
+++ core/src/main/java/com/core/domain/auction/auction_field/PurchaseTime.java
@@ -0,0 +1,17 @@
+package com.core.domain.auction.auction_field;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public enum PurchaseTime {
+	UNDER_ONE_MONTH("1개월 이하"),
+	UNDER_THREE_MONTH("3개월 이하"),
+	UNDER_SIX_MONTH("6개월 이하"),
+	UNDER_ONE_YEAR("1년 이하"),
+	ABOVE_ONE_YEAR("1년 이상"),
+	UNKNOWN("모름");
+
+	private final String description;
+}
