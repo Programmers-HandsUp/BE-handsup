1b8c38ffbccf9e1da6ef60447ea1c3103229d76a
ParkJuHan94 <1004sunye@naver.com>
2024-02-16 01:54:04 +0900

    [feat] : 암호화 엔티티 추

---
+++ core/src/main/java/dev/handsup/auth/domain/BcryptImpl.java
@@ -0,0 +1,18 @@
+package dev.handsup.auth.domain;
+
+import org.mindrot.jbcrypt.BCrypt;
+import org.springframework.stereotype.Component;
+
+@Component
+public class BcryptImpl implements EncryptHelper {
+
+	@Override
+	public String encrypt(String plainPassword) {
+		return BCrypt.hashpw(plainPassword, BCrypt.gensalt());
+	}
+
+	@Override
+	public boolean isMatch(String plainPassword, String hashedPassword) {
+		return BCrypt.checkpw(plainPassword, hashedPassword);
+	}
+}
