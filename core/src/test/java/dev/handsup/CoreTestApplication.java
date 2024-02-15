049be641332b58fbd4480fa4333983f60764ddc8
hs12 <loveanna1228@gmail.com>
2024-02-15 10:44:04 +0900

    [feat] : testcontainer 테스트 클래스 추가

---
+++ core/src/test/java/dev/handsup/CoreTestApplication.java
@@ -0,0 +1,12 @@
+package dev.handsup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoreTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoreTestApplication.class, args);
+	}
+}
---
+++ core/src/test/java/dev/handsup/HandsUpApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.handsup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import dev.handsup.support.TestContainerSupport;
+
+@SpringBootTest
+class HandsUpApplicationTests extends TestContainerSupport {
+	@Test
+	void contextLoads() {
+	}
+}
