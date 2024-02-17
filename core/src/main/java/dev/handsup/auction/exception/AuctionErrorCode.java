76de873470c47bb0c28ae7a87a0a8f2a41728504
hs12 <loveanna1228@gmail.com>
2024-02-17 21:43:03 +0900

    [feat] : auctionErrorCode 추가 및 불필요한 코드 삭제

--- core/src/main/java/dev/handsup/auction/exception/AuctionErrorCode.java
+++ core/src/main/java/dev/handsup/auction/exception/AuctionErrorCode.java
@@ -11,7 +11,7 @@ public enum AuctionErrorCode implements ErrorCode {
 	NOT_FOUND_PRODUCT_STATUS("올바른 상품 상태를 입력해주세요", "P_001"),
 	NOT_FOUND_PURCHASE_TIME("올바른 구매 시기를 입력해주세요", "P_002" ),
 	NOT_FOUND_TADE_METHOD("올바른 거래 방법을 입력해주세요","P_003" ),
-	NOT_FOUND_PRODUCT_CATEGORY("올바른 상품 카테고리를 입력해주세요","P_004" );
+	NOT_FOUND_PRODUCT_CATEGORY("DB에 해당 상품 카테고리가 존재하지 않습니다.", "P_005");
 
 	private final String message;
 	private final String code;
