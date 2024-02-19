c3aa2041b137a203d1c661b08adb39d6d41004f7
hs12 <loveanna1228@gmail.com>
2024-02-19 11:40:42 +0900

    [feat] : 경매 예외 테스트 케이스 추가

--- core/src/main/java/dev/handsup/auction/exception/AuctionErrorCode.java
+++ core/src/main/java/dev/handsup/auction/exception/AuctionErrorCode.java
@@ -11,7 +11,7 @@ public enum AuctionErrorCode implements ErrorCode {
 	NOT_FOUND_PRODUCT_STATUS("올바른 상품 상태를 입력해주세요", "AU_001"),
 	NOT_FOUND_PURCHASE_TIME("올바른 구매 시기를 입력해주세요", "AU_002"),
 	NOT_FOUND_TADE_METHOD("올바른 거래 방법을 입력해주세요", "AU_003"),
-	NOT_FOUND_PRODUCT_CATEGORY("DB에 해당 상품 카테고리가 존재하지 않습니다.", "AU_004");
+	NOT_FOUND_PRODUCT_CATEGORY("존재하지 않는 상품 카테고리입니다.", "AU_004");
 
 	private final String message;
 	private final String code;
