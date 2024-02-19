f18a48c406d8e746ee08195cb7b537183821146d
hs12 <loveanna1228@gmail.com>
2024-02-19 11:10:55 +0900

    [style] : 경매 에러코드 수정

--- core/src/main/java/dev/handsup/auction/exception/AuctionErrorCode.java
+++ core/src/main/java/dev/handsup/auction/exception/AuctionErrorCode.java
@@ -8,10 +8,10 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public enum AuctionErrorCode implements ErrorCode {
 
-	NOT_FOUND_PRODUCT_STATUS("올바른 상품 상태를 입력해주세요", "P_001"),
-	NOT_FOUND_PURCHASE_TIME("올바른 구매 시기를 입력해주세요", "P_002"),
-	NOT_FOUND_TADE_METHOD("올바른 거래 방법을 입력해주세요", "P_003"),
-	NOT_FOUND_PRODUCT_CATEGORY("DB에 해당 상품 카테고리가 존재하지 않습니다.", "P_005");
+	NOT_FOUND_PRODUCT_STATUS("올바른 상품 상태를 입력해주세요", "AU_001"),
+	NOT_FOUND_PURCHASE_TIME("올바른 구매 시기를 입력해주세요", "AU_002"),
+	NOT_FOUND_TADE_METHOD("올바른 거래 방법을 입력해주세요", "AU_003"),
+	NOT_FOUND_PRODUCT_CATEGORY("DB에 해당 상품 카테고리가 존재하지 않습니다.", "AU_004");
 
 	private final String message;
 	private final String code;
