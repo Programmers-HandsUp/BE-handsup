2f712dc6f5859d4dcc04818a48003358a82e6017
hs12 <loveanna1228@gmail.com>
2024-02-19 11:39:28 +0900

    [feat] : 통합 테스트용 databaseCleaner 추가

--- api/src/test/java/dev/handsup/common/support/ApiTestSupport.java
+++ api/src/test/java/dev/handsup/common/support/ApiTestSupport.java
@@ -1,17 +1,23 @@
 package dev.handsup.common.support;
 
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
 import org.springframework.test.web.servlet.MockMvc;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
+import dev.handsup.support.DatabaseCleaner;
+import dev.handsup.support.DatabaseCleanerExtension;
 import dev.handsup.support.TestContainerSupport;
 
 @SpringBootTest
 @AutoConfigureMockMvc
+@Import(DatabaseCleaner.class)
+@ExtendWith(DatabaseCleanerExtension.class)
 public abstract class ApiTestSupport extends TestContainerSupport {
 
 	@Autowired
