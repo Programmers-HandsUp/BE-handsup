54abaf9937003f799bf8cae177a9aeecebab68fd
hs12 <loveanna1228@gmail.com>
2024-02-20 19:40:23 +0900

    [feat] : respository support class 추가

--- core/src/test/java/dev/handsup/support/DataJpaTestSupport.java
+++ core/src/test/java/dev/handsup/support/DataJpaTestSupport.java
@@ -2,8 +2,18 @@ package dev.handsup.support;
 
 import static org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace.*;
 
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Import;
+import org.springframework.stereotype.Repository;
 
+import dev.handsup.common.config.QueryDslConfig;
+
+@DataJpaTest(includeFilters = @ComponentScan.Filter(Repository.class))
+@Import({DatabaseCleaner.class, QueryDslConfig.class})
 @AutoConfigureTestDatabase(replace = NONE)
+@ExtendWith(DatabaseCleanerExtension.class)
 public abstract class DataJpaTestSupport extends TestContainerSupport {
 }
