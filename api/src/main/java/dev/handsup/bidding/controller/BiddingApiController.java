af3fdf3c905264679f77bdd92114eb0db5bfab92
ParkJuHan94 <1004sunye@naver.com>
2024-02-23 04:02:45 +0900

    [feat] : BiddingApiController 와 테스트 구현

--- api/src/main/java/dev/handsup/bidding/controller/BiddingApiController.java
+++ api/src/main/java/dev/handsup/bidding/controller/BiddingApiController.java
@@ -1,7 +1,5 @@
 package dev.handsup.bidding.controller;
 
-import java.util.Optional;
-
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -9,11 +7,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import dev.handsup.auction.domain.Auction;
-import dev.handsup.auction.repository.AuctionRepository;
 import dev.handsup.auth.jwt.JwtAuthorization;
-import dev.handsup.bidding.dto.BiddingApiMapper;
-import dev.handsup.bidding.dto.RegisterBiddingApiRequest;
 import dev.handsup.bidding.dto.request.RegisterBiddingRequest;
 import dev.handsup.bidding.dto.response.RegisterBiddingResponse;
 import dev.handsup.bidding.service.BiddingService;
@@ -22,7 +16,8 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import jakarta.validation.Valid;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.NotNull;
 import lombok.RequiredArgsConstructor;
 
 @Tag(name = "Bidding API")
@@ -32,31 +27,24 @@ import lombok.RequiredArgsConstructor;
 public class BiddingApiController {
 
 	private final BiddingService biddingService;
-	//TODO 추후 AuctionService의 조회메서드로 변경
-	private final AuctionRepository auctionRepository;
 
 	@PostMapping("/{auctionId}/bids")
 	@Operation(summary = "입찰 등록 API", description = "입찰한다")
 	@ApiResponse(useReturnTypeSchema = true)
 	public ResponseEntity<RegisterBiddingResponse> registerBidding(
 		@PathVariable Long auctionId,
-		@Valid @RequestBody RegisterBiddingApiRequest request,
+		@RequestBody
+		@NotNull(message = "biddingPrice 값이 공백입니다.")
+		@Max(value = 1_000_000_000, message = "최대 입찰가는 10억입니다.")
+		int biddingPrice,
 		@Parameter(hidden = true) @JwtAuthorization User user
 	) {
-		Optional<Auction> auction = auctionRepository.findById(auctionId);
-
-		if(auction.isPresent()) {
-			RegisterBiddingRequest registerBiddingRequest = BiddingApiMapper.toRegisterBiddingRequest(
-				request,
-				auction.get(),
-				user
-			);
-			RegisterBiddingResponse response = biddingService.registerBidding(
-				registerBiddingRequest
-			);
-			return ResponseEntity.ok(response);
-		}
-
-		return ResponseEntity.badRequest().build();
+		RegisterBiddingRequest request = RegisterBiddingRequest.of(
+			biddingPrice,
+			auctionId,
+			user
+		);
+		RegisterBiddingResponse response = biddingService.registerBidding(request);
+		return ResponseEntity.ok(response);
 	}
 }
--- api/src/test/java/dev/handsup/bidding/controller/BiddingApiControllerTest.java
+++ api/src/test/java/dev/handsup/bidding/controller/BiddingApiControllerTest.java
@@ -1,26 +1,26 @@
 package dev.handsup.bidding.controller;
 
 import static org.springframework.http.HttpHeaders.*;
+import static org.springframework.http.MediaType.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 import dev.handsup.auction.domain.Auction;
 import dev.handsup.auction.domain.product.product_category.ProductCategory;
-import dev.handsup.auction.repository.AuctionRepository;
-import dev.handsup.auction.repository.ProductCategoryRepository;
-import dev.handsup.bidding.dto.RegisterBiddingApiRequest;
+import dev.handsup.auction.repository.auction.AuctionRepository;
+import dev.handsup.auction.repository.product.ProductCategoryRepository;
 import dev.handsup.common.support.ApiTestSupport;
 import dev.handsup.fixture.AuctionFixture;
 import dev.handsup.fixture.ProductFixture;
 import dev.handsup.fixture.UserFixture;
 import dev.handsup.user.domain.User;
+import dev.handsup.user.repository.UserRepository;
 
 @DisplayName("[BiddingApiController 테스트]")
 class BiddingApiControllerTest extends ApiTestSupport {
@@ -29,38 +29,38 @@ class BiddingApiControllerTest extends ApiTestSupport {
 	private AuctionRepository auctionRepository;
 	@Autowired
 	private ProductCategoryRepository productCategoryRepository;
+	@Autowired
+	private UserRepository userRepository;
 	private final String DIGITAL_DEVICE = "디지털 기기";
-
-	private User user = UserFixture.user();
-	private Auction auction = AuctionFixture.auction();
-	private RegisterBiddingApiRequest request = RegisterBiddingApiRequest.from(10000);
+	private final User user = UserFixture.user();
+	private Auction auction;
 
 	@BeforeEach
 	void setUp() {
 		ProductCategory productCategory = ProductFixture.productCategory(DIGITAL_DEVICE);
 		productCategoryRepository.save(productCategory);
+		auction =  auctionRepository.save(AuctionFixture.auction(productCategory));
+		// 전체 테스트시 user 가 db 에서 삭제되는 오류
+		userRepository.save(user);
 	}
 
 	@Test
 	@DisplayName("[입찰 등록 API를 호출하면 입찰이 등록되고 입찰을 응답한다]")
 	void registerBiddingTest() throws Exception {
-		//given
-		auctionRepository.save(auction);
-
 		// when
 		ResultActions resultActions = mockMvc.perform(
 			MockMvcRequestBuilders
-				.post("/api/auctions/" + auction.getId() + "/bids")
+				.post("/api/auctions/{auctionId}/bids", auction.getId())
 				.header(AUTHORIZATION, accessToken)
-				.contentType(MediaType.APPLICATION_JSON)
-				.content(toJson(request))
+				.contentType(APPLICATION_JSON)
+				.content(toJson(10000))
 		);
 
 		// then
 		resultActions.andExpectAll(
 			status().isOk(),
-
 			jsonPath("$.biddingPrice").value(10000),
+			jsonPath("$.auctionId").value(auction.getId()),
 			jsonPath("$.bidderId").value(user.getId())
 		);
 	}
