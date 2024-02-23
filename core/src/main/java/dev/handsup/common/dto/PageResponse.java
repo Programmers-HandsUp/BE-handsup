3f2ddcec9de797b6089104822ae7925111eb7608
hs12 <loveanna1228@gmail.com>
2024-02-23 10:45:39 +0900

    [feat] : pageResponse 내 팩토리 메서드 추가

--- core/src/main/java/dev/handsup/common/dto/PageResponse.java
+++ core/src/main/java/dev/handsup/common/dto/PageResponse.java
@@ -2,9 +2,18 @@ package dev.handsup.common.dto;
 
 import java.util.List;
 
+import org.springframework.data.domain.Slice;
+
 public record PageResponse<T>(
 	List<T> content,
 	long size,
 	boolean hasNext
 ) {
+	public static <T> PageResponse<T> of(Slice<T> page) {
+		return new PageResponse<>(
+			page.getContent(),
+			page.getNumberOfElements(),
+			page.hasNext()
+		);
+	}
 }
