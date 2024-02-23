e439d9273bd1507f0a11fbc7b3972b6e20f8f3fc
hs12 <loveanna1228@gmail.com>
2024-02-23 20:22:33 +0900

    [feat] : AuctionSimpleResponse 추가

---
+++ core/src/main/java/dev/handsup/auction/dto/response/AuctionSimpleResponse.java
@@ -0,0 +1,31 @@
+package dev.handsup.auction.dto.response;
+
+public record AuctionSimpleResponse(
+	Long auctionId,
+	String title,
+	int initPrice,
+	int bookmarkCount,
+	String dong,
+	String createdDate,
+	String profileImgUrl
+) {
+	public static AuctionSimpleResponse from(
+		Long auctionId,
+		String title,
+		int initPrice,
+		int bookmarkCount,
+		String dong,
+		String createDate,
+		String profileImgUrl
+	){
+		return new AuctionSimpleResponse(
+			auctionId,
+			title,
+			initPrice,
+			bookmarkCount,
+			dong,
+			createDate,
+			profileImgUrl
+		);
+	}
+}
