4da3a828b43c4a178797458b6017b60b25fe2bbf
hs12 <loveanna1228@gmail.com>
2024-02-23 10:21:50 +0900

    [feat] : bookmark repository 추가

---
+++ core/src/main/java/dev/handsup/auction/repository/auction/BookmarkRepository.java
@@ -0,0 +1,14 @@
+package dev.handsup.auction.repository.auction;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import dev.handsup.auction.domain.Auction;
+import dev.handsup.auction.domain.Bookmark;
+import dev.handsup.user.domain.User;
+
+public interface BookmarkRepository extends JpaRepository<Bookmark, Long> {
+	Optional<Bookmark> findByUserAndAuction(User user, Auction auction);
+	Boolean existsByUserAndAuction(User user, Auction auction);
+}
