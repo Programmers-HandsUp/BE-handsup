58c9b63ecbdb260add1c87829fdbd15dac1cf718
hs12 <loveanna1228@gmail.com>
2024-02-23 22:22:36 +0900

    [feat] : 팩토리 메서드 추가 및 북마크 수 필드 추가

--- core/src/main/java/dev/handsup/auction/dto/response/AuctionDetailResponse.java
+++ core/src/main/java/dev/handsup/auction/dto/response/AuctionDetailResponse.java
@@ -1,31 +1,39 @@
 package dev.handsup.auction.dto.response;
 
-import java.time.LocalDate;
-
-import lombok.Builder;
-
-@Builder
 public record AuctionDetailResponse(
 
 	Long auctionId,
 	String title,
-
 	String productCategory,
-
 	int initPrice,
-
-	LocalDate endDate,
-
+	String endDate,
 	String productStatus,
-
 	String purchaseTime,
-
 	String description,
-
 	String tradeMethod,
-
 	String si,
 	String gu,
-	String dong
+	String dong,
+
+	int bookmarkCount
 ) {
+	public static AuctionDetailResponse of(
+		Long auctionId,
+		String title,
+		String productCategory,
+		int initPrice,
+		String endDate,
+		String productStatus,
+		String purchaseTime,
+		String description,
+		String tradeMethod,
+		String si,
+		String gu,
+		String dong,
+		int bookmarkCount
+	) {
+		return new AuctionDetailResponse(
+			auctionId, title, productCategory, initPrice, endDate, productStatus, purchaseTime, description,
+			tradeMethod, si, gu, dong, bookmarkCount);
+	}
 }
