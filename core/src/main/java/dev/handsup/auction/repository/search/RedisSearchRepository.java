419b2e78af2f7f01a848e9b2e3ba47f5af4ca98e
hs12 <loveanna1228@gmail.com>
2024-02-25 16:45:34 +0900

    [feat] : search redis repository 검색 수 추가 반환

--- core/src/main/java/dev/handsup/auction/repository/search/RedisSearchRepository.java
+++ core/src/main/java/dev/handsup/auction/repository/search/RedisSearchRepository.java
@@ -2,11 +2,15 @@ package dev.handsup.auction.repository.search;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 import java.util.Set;
 
 import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.core.ZSetOperations;
 import org.springframework.stereotype.Component;
 
+import dev.handsup.search.domain.KeywordType;
+import dev.handsup.search.dto.PopularKeywordResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
@@ -15,31 +19,45 @@ import lombok.extern.slf4j.Slf4j;
 @Component
 public class RedisSearchRepository{
 	private final StringRedisTemplate redisTemplate;
-	private static final String KEY = "RANKING";
 
-	public List<String> getPopularKeywords(){
-		Set<String> ranking = redisTemplate.opsForZSet()
-			.reverseRange(KEY, 0, 9);
-		if (ranking == null){
-			return new ArrayList<>();
+	public List<PopularKeywordResponse> getPopularKeywords() {
+		// 인기순 검색어, 검색 수 조회
+		Set<ZSetOperations.TypedTuple<String>> typedTuples = getKeywordsWithScore(KeywordType.POPULAR.getKey());
+
+		// set(typedTuple) -> list(response) 로 변환
+		List<PopularKeywordResponse> popularKeywords = new ArrayList<>();
+
+		if (typedTuples != null) {
+			for (ZSetOperations.TypedTuple<String> typedTuple : typedTuples) {
+				String keyword = typedTuple.getValue();
+				int count = Objects.requireNonNull(typedTuple.getScore()).intValue();
+
+				popularKeywords.add(PopularKeywordResponse.of(keyword, count));
+			}
 		}
-		return new ArrayList<>(ranking);
+
+		return popularKeywords;
+	}
+
+	public Set<ZSetOperations.TypedTuple<String>> getKeywordsWithScore(String key){
+		return redisTemplate.opsForZSet()
+			.reverseRangeWithScores(key, 0, 9);
 	}
 
 	public void increaseSearchCount(String keyword){
 		int increaseAmount = 1;
 		redisTemplate.opsForZSet()
-			.incrementScore(KEY, keyword, increaseAmount);
+			.incrementScore(KeywordType.POPULAR.getKey(), keyword, increaseAmount);
 	}
 
 	//==테스트용 함수==//
 	public void increaseSearchCount(String keyword, int increaseAmount){
 		redisTemplate.opsForZSet()
-			.incrementScore(KEY, keyword, increaseAmount);
+			.incrementScore(KeywordType.POPULAR.getKey(), keyword, increaseAmount);
 	}
 
 	public int getKeywordCount(String keyword){
-		Double score = redisTemplate.opsForZSet().score(KEY, keyword);
+		Double score = redisTemplate.opsForZSet().score(KeywordType.POPULAR.getKey(), keyword);
 		if (score==null) return 0; // 검색한 적 없을 때
 		else
 			return (int) Math.round(score);
