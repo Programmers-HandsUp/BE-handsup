fdec759f9d4bc94506d7ee468a008246570181e6
ParkJuHan94 <1004sunye@naver.com>
2024-02-25 10:43:50 +0900

    [feat] : 입찰 목록 전체 조회 Api 개발

--- api/src/main/java/dev/handsup/bidding/controller/BiddingApiController.java
+++ api/src/main/java/dev/handsup/bidding/controller/BiddingApiController.java
@@ -1,16 +1,20 @@
 package dev.handsup.bidding.controller;
 
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import dev.handsup.auth.annotation.NoAuth;
 import dev.handsup.auth.jwt.JwtAuthorization;
 import dev.handsup.bidding.dto.request.RegisterBiddingRequest;
 import dev.handsup.bidding.dto.response.BiddingResponse;
 import dev.handsup.bidding.service.BiddingService;
+import dev.handsup.common.dto.PageResponse;
 import dev.handsup.user.domain.User;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
@@ -28,7 +32,7 @@ public class BiddingApiController {
 	private final BiddingService biddingService;
 
 	@PostMapping("/{auctionId}/bids")
-	@Operation(summary = "입찰 등록 API", description = "입찰한다")
+	@Operation(summary = "입찰 등록 API", description = "사용자가 경매에 입찰을 등록한다")
 	@ApiResponse(useReturnTypeSchema = true)
 	public ResponseEntity<BiddingResponse> registerBidding(
 		@PathVariable Long auctionId,
@@ -39,4 +43,15 @@ public class BiddingApiController {
 		return ResponseEntity.ok(response);
 	}
 
+	@NoAuth
+	@GetMapping("/{auctionId}/bids")
+	@Operation(summary = "입찰 목록 전체 조회 API", description = "한 경매의 모든 입찰 목록을 입찰가 기준 내림차순으로 조회한다")
+	@ApiResponse(useReturnTypeSchema = true)
+	public ResponseEntity<PageResponse<BiddingResponse>> getBidsOfAuction(
+		@PathVariable Long auctionId,
+		Pageable pageable
+	) {
+		PageResponse<BiddingResponse> response = biddingService.getBidsOfAuction(auctionId, pageable);
+		return ResponseEntity.ok(response);
+	}
 }
--- core/src/main/java/dev/handsup/bidding/dto/BiddingMapper.java
+++ core/src/main/java/dev/handsup/bidding/dto/BiddingMapper.java
@@ -2,14 +2,33 @@ package dev.handsup.bidding.dto;
 
 import static lombok.AccessLevel.*;
 
+import org.springframework.data.domain.Slice;
+
 import dev.handsup.bidding.domain.Bidding;
 import dev.handsup.bidding.dto.response.BiddingResponse;
+import dev.handsup.common.dto.PageResponse;
 import lombok.NoArgsConstructor;
 
 @NoArgsConstructor(access = PRIVATE)
 public class BiddingMapper {
 
 	public static BiddingResponse toRegisterBiddingResponse(Bidding bidding) {
+		return BiddingResponse.of(
+			bidding.getBiddingPrice(),
+			bidding.getAuction().getId(),
+			bidding.getBidder().getNickname()
+		);
+	}
+
+	public static <T> PageResponse<T> toBiddingPageResponse(Slice<T> page) {
+		return new PageResponse<>(
+			page.getContent(),
+			page.getNumberOfElements(),
+			page.hasNext()
+		);
+	}
+
+	public static BiddingResponse toBiddingResponse(Bidding bidding) {
 		return BiddingResponse.of(
 			bidding.getBiddingPrice(),
 			bidding.getAuction().getId(),
