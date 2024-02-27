f73ba927f07cb9881e219bf6ba41218b8e04ac00
hs12 <loveanna1228@gmail.com>
2024-02-27 20:47:05 +0900

    [feat] : 이미지 관련 에러코드 추가

---
+++ core/src/main/java/dev/handsup/image/exception/ImageErrorCode.java
@@ -0,0 +1,18 @@
+package dev.handsup.image.exception;
+
+import dev.handsup.common.exception.ErrorCode;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public enum ImageErrorCode implements ErrorCode {
+
+	INVALID_FILE_NAME("파일명이 공백입니다.", "IMG_001"),
+	INVALID_FILE_EXTENSION("잘못된 파일 형식입니다.", "IMG_002"),
+	FAILED_TO_UPLOAD("s3에 이미지를 업로드하는데 실패했습니다.", "IMG_003"),
+	FAILED_TO_REMOVE("s3에서 이미지를 제거하는데 실패했습니다.", "IMG_004");
+
+	private final String message;
+	private final String code;
+}
