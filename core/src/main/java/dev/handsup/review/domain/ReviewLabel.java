8fc88e2b210a6fc897df288aa13404a2c5e251c6
ParkJuHan94 <1004sunye@naver.com>
2024-02-27 18:25:17 +0900

    [feat] : 리뷰라벨 엔티티

--- core/src/main/java/dev/handsup/review/domain/ReviewLabel.java
+++ core/src/main/java/dev/handsup/review/domain/ReviewLabel.java
@@ -9,6 +9,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -25,4 +26,25 @@ public class ReviewLabel {
 	@Column(name = "review_label_value", nullable = false)
 	@Enumerated(STRING)
 	private ReviewLabelValue value;
+
+	@Builder
+	private ReviewLabel(ReviewLabelValue value) {
+		this.value = value;
+	}
+
+	public static ReviewLabel from(ReviewLabelValue value) {
+		return ReviewLabel.builder()
+			.value(value)
+			.build();
+	}
+
+	//==테스트용 생성자==//
+	private ReviewLabel(Long id, ReviewLabelValue value) {
+		this.id = id;
+		this.value = value;
+	}
+
+	public static ReviewLabel getTestReviewLabel(Long id, ReviewLabelValue value) {
+		return new ReviewLabel(id, value);
+	}
 }
