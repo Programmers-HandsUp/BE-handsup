24700c351c2c08f64c988867c8da260db7421589
ParkJuHan94 <1004sunye@naver.com>
2024-03-03 09:29:30 +0900

    [feat] : 후기 등록 service 메서드 보완과 테스트 추가

--- core/src/main/java/dev/handsup/review/service/ReviewService.java
+++ core/src/main/java/dev/handsup/review/service/ReviewService.java
@@ -1,13 +1,15 @@
 package dev.handsup.review.service;
 
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import dev.handsup.auction.domain.Auction;
-import dev.handsup.auction.service.AuctionService;
+import dev.handsup.auction.repository.auction.AuctionRepository;
 import dev.handsup.common.service.EntityManagementService;
 import dev.handsup.review.domain.Review;
 import dev.handsup.review.domain.ReviewInterReviewLabel;
 import dev.handsup.review.domain.ReviewLabel;
+import dev.handsup.review.domain.UserReviewLabel;
 import dev.handsup.review.dto.ReviewMapper;
 import dev.handsup.review.dto.request.RegisterReviewRequest;
 import dev.handsup.review.dto.response.ReviewResponse;
@@ -15,6 +17,7 @@ import dev.handsup.review.repository.ReviewInterReviewLabelRepository;
 import dev.handsup.review.repository.ReviewLabelRepository;
 import dev.handsup.review.repository.ReviewRepository;
 import dev.handsup.user.domain.User;
+import dev.handsup.user.repository.UserReviewLabelRepository;
 import lombok.RequiredArgsConstructor;
 
 @Service
@@ -24,15 +27,17 @@ public class ReviewService {
 	private final ReviewRepository reviewRepository;
 	private final ReviewLabelRepository reviewLabelRepository;
 	private final ReviewInterReviewLabelRepository reviewInterReviewLabelRepository;
-	private final AuctionService auctionService;
+	private final UserReviewLabelRepository userReviewLabelRepository;
+	private final AuctionRepository auctionRepository;
 	private final EntityManagementService entityManagementService;
 
+	@Transactional
 	public ReviewResponse registerReview(
 		RegisterReviewRequest request,
 		Long auctionId,
 		User writer
 	) {
-		Auction auction = auctionService.getAuctionEntity(auctionId);
+		Auction auction = entityManagementService.getEntity(auctionRepository, auctionId);
 		Review review = reviewRepository.save(
 			ReviewMapper.toReview(request, auction, writer)
 		);
@@ -44,6 +49,16 @@ public class ReviewService {
 			reviewInterReviewLabelRepository.save(
 				ReviewInterReviewLabel.of(review, reviewLabel)
 			);
+
+			// UserReviewLabel 카운팅
+			UserReviewLabel userReviewLabel = userReviewLabelRepository.save(
+				UserReviewLabel.of(reviewLabel, auction.getSeller())
+			);
+			Long userReviewLabelId = userReviewLabel.getId();
+			int userReviewLabelCount = entityManagementService.getEntity(
+				userReviewLabelRepository, userReviewLabelId
+			).getCount();
+			userReviewLabelRepository.updateCount(userReviewLabelId, userReviewLabelCount + 1);
 		});
 
 		return ReviewMapper.toReviewResponse(review);
