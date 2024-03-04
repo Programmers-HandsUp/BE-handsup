7c983b249a848af724c41f850d6b320c4404648a
ParkJuHan94 <1004sunye@naver.com>
2024-03-04 21:23:39 +0900

    [refactor] : JoinUserRequest 단일화 - 2

--- api/src/main/java/dev/handsup/user/controller/UserApiController.java
+++ api/src/main/java/dev/handsup/user/controller/UserApiController.java
@@ -6,8 +6,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import dev.handsup.auth.annotation.NoAuth;
-import dev.handsup.user.dto.JoinUserApiRequest;
-import dev.handsup.user.dto.UserApiMapper;
 import dev.handsup.user.dto.request.JoinUserRequest;
 import dev.handsup.user.dto.response.JoinUserResponse;
 import dev.handsup.user.service.UserService;
@@ -29,10 +27,9 @@ public class UserApiController {
 	@Operation(summary = "회원가입 API", description = "회원가입을 한다")
 	@ApiResponse(useReturnTypeSchema = true)
 	public ResponseEntity<JoinUserResponse> join(
-		final @Valid @RequestBody JoinUserApiRequest request
+		final @Valid @RequestBody JoinUserRequest request
 	) {
-		JoinUserRequest joinUserRequest = UserApiMapper.toJoinUserRequest(request);
-		Long userId = userService.join(joinUserRequest);
+		Long userId = userService.join(request);
 		JoinUserResponse response = JoinUserResponse.from(userId);
 		return ResponseEntity.ok(response);
 	}
