fb607268c747014b6a155e04d8d8f0b3364b66ed
hs12 <loveanna1228@gmail.com>
2024-03-04 22:57:22 +0900

    [feat] : 유저로 채팅방 조회하는 repository 함수 추가

--- core/src/main/java/dev/handsup/chat/repository/ChatRoomRepository.java
+++ core/src/main/java/dev/handsup/chat/repository/ChatRoomRepository.java
@@ -1,8 +1,14 @@
 package dev.handsup.chat.repository;
 
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import dev.handsup.chat.domain.ChatRoom;
+import dev.handsup.user.domain.User;
 
 public interface ChatRoomRepository extends JpaRepository<ChatRoom, Long> {
+	@Query("SELECT cr FROM ChatRoom cr WHERE cr.seller = :user OR cr.buyer = :user")
+	Slice<ChatRoom> findChatRoomsByUser(User user, Pageable pageable);
 }
