d7ce898370d7065c4c43284ee9506474bfbbb576
hs12 <loveanna1228@gmail.com>
2024-03-04 22:59:36 +0900

    [feat] : 유저 채팅방 조회 controller 메서드 작성

--- api/src/main/java/dev/handsup/controller/ChatRoomApiController.java
+++ api/src/main/java/dev/handsup/controller/ChatRoomApiController.java
@@ -1,15 +1,18 @@
 package dev.handsup.controller;
 
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import dev.handsup.auth.jwt.JwtAuthorization;
-import dev.handsup.chat.dto.request.RegisterChatRoomResponse;
+import dev.handsup.chat.dto.response.ChatRoomSimpleResponse;
+import dev.handsup.chat.dto.response.RegisterChatRoomResponse;
 import dev.handsup.chat.service.ChatRoomService;
+import dev.handsup.common.dto.PageResponse;
 import dev.handsup.user.domain.User;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
@@ -21,29 +24,30 @@ import lombok.RequiredArgsConstructor;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/chatrooms")
-public class ChatRoomController {
+public class ChatRoomApiController {
 	private final ChatRoomService chatRoomService;
 
 	@Operation(summary = "채팅방 등록 API", description = "채팅방을 등록한다")
 	@ApiResponse(useReturnTypeSchema = true)
 	@PostMapping
-	public ResponseEntity<RegisterChatRoomResponse> registerChatroom(
-		@Parameter(hidden = true) @JwtAuthorization User seller,
-		@Parameter Long buyerId
+	public ResponseEntity<RegisterChatRoomResponse> registerChatRoom(
+		@RequestParam("auctionId") Long auctionId,
+		@RequestParam("buyerId") Long buyerId,
+		@Parameter(hidden = true) @JwtAuthorization User seller
 	) {
-		RegisterChatRoomResponse response = chatRoomService.registerChatRoom(seller, buyerId);
+		RegisterChatRoomResponse response = chatRoomService.registerChatRoom(auctionId, buyerId, seller);
 		return ResponseEntity.ok(response);
 	}
 
 
-	@Operation(summary = "채팅방 조회 API", description = "채팅방을 조회한다")
+	@Operation(summary = "유저 채팅방 모두 조회 API", description = "유저 아이디로 채팅방 목록을 모두 조회한다.")
 	@ApiResponse(useReturnTypeSchema = true)
-	@GetMapping("/{chatRoomId}")
-	public ResponseEntity<RegisterChatRoomResponse> getChatroom(
-		@Parameter(hidden = true) @JwtAuthorization User seller,
-		@PathVariable("chatRoomId") Long chatRoomId
+	@GetMapping
+	public ResponseEntity<PageResponse<ChatRoomSimpleResponse>> getUserChatRooms(
+		@Parameter(hidden = true) @JwtAuthorization User user,
+		Pageable pageable
 	) {
-		RegisterChatRoomResponse response = chatRoomService.getChatRoom(seller, buyerId);
+		PageResponse<ChatRoomSimpleResponse> response = chatRoomService.getUserChatRooms(user, pageable);
 		return ResponseEntity.ok(response);
 	}
 }
--- core/src/main/java/dev/handsup/chat/dto/request/RegisterChatRoomResponse.java
+++
@@ -1,9 +0,0 @@
-package dev.handsup.chat.dto.request;
-
-public record RegisterChatRoomResponse(
-	Long chatRoomId
-){
-	public static RegisterChatRoomResponse of(Long chatRoomId){
-		return new RegisterChatRoomResponse(chatRoomId);
-	}
-}
