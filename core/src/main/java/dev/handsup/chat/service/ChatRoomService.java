ab6750b7af86ca1606151d2986b3f8f5f1cf0b28
hs12 <loveanna1228@gmail.com>
2024-03-04 22:58:56 +0900

    [feat] : 유저 채팅방 조회 비즈니스 로직 작성

--- core/src/main/java/dev/handsup/chat/service/ChatRoomService.java
+++ core/src/main/java/dev/handsup/chat/service/ChatRoomService.java
@@ -1,11 +1,16 @@
 package dev.handsup.chat.service;
 
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import dev.handsup.chat.domain.ChatRoom;
 import dev.handsup.chat.dto.ChatMapper;
-import dev.handsup.chat.dto.request.RegisterChatRoomResponse;
+import dev.handsup.chat.dto.response.ChatRoomSimpleResponse;
+import dev.handsup.chat.dto.response.RegisterChatRoomResponse;
 import dev.handsup.chat.repository.ChatRoomRepository;
+import dev.handsup.common.dto.PageResponse;
 import dev.handsup.common.exception.NotFoundException;
 import dev.handsup.user.domain.User;
 import dev.handsup.user.exception.UserErrorCode;
@@ -18,9 +23,19 @@ public class ChatRoomService {
 	private final ChatRoomRepository chatRoomRepository;
 	private final UserRepository userRepository;
 
-	public RegisterChatRoomResponse registerChatRoom(User seller, Long buyerId ){
-		ChatRoom chatRoom = ChatMapper.toChatRoom(seller, getBuyer(buyerId));
+	public RegisterChatRoomResponse registerChatRoom(Long auctionId, Long buyerId, User seller){
+		ChatRoom chatRoom = ChatMapper.toChatRoom(auctionId, seller, getBuyer(buyerId));
 		return ChatMapper.toRegisterChatRoomResponse(chatRoomRepository.save(chatRoom));
+	}
+
+	@Transactional(readOnly = true)
+	public PageResponse<ChatRoomSimpleResponse> getUserChatRooms(User user, Pageable pageable) {
+		Slice<ChatRoomSimpleResponse> chatRoomResponses = chatRoomRepository.findChatRoomsByUser(user, pageable)
+			.map(chatRoom -> {
+				User receiver = chatRoom.getBuyer().equals(user) ? chatRoom.getSeller() : chatRoom.getBuyer();
+				return ChatMapper.toChatRoomSimpleResponse(chatRoom, receiver);
+			});
+		return ChatMapper.toPageResponse(chatRoomResponses);
 	}
 
 	private User getBuyer(Long buyerId) {
