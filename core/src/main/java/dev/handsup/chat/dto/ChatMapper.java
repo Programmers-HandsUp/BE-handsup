e440fd177c24f4df7ab49c338faf932008739917
hs12 <loveanna1228@gmail.com>
2024-03-04 22:58:21 +0900

    [feat] : ChatMapper에 간단 채팅방 조회 dto 매핑 함수 추가

--- core/src/main/java/dev/handsup/chat/dto/ChatMapper.java
+++ core/src/main/java/dev/handsup/chat/dto/ChatMapper.java
@@ -2,18 +2,30 @@ package dev.handsup.chat.dto;
 
 import static lombok.AccessLevel.*;
 
+import org.springframework.data.domain.Slice;
+
 import dev.handsup.chat.domain.ChatRoom;
-import dev.handsup.chat.dto.request.RegisterChatRoomResponse;
+import dev.handsup.chat.dto.response.ChatRoomSimpleResponse;
+import dev.handsup.chat.dto.response.RegisterChatRoomResponse;
+import dev.handsup.common.dto.PageResponse;
 import dev.handsup.user.domain.User;
 import lombok.NoArgsConstructor;
 
 @NoArgsConstructor(access = PRIVATE)
 public class ChatMapper {
-	public static ChatRoom toChatRoom(User seller, User buyer){
-		return ChatRoom.of(seller, buyer);
+	public static ChatRoom toChatRoom(Long auctionId, User seller, User buyer){
+		return ChatRoom.of(auctionId, seller, buyer);
 	}
 
 	public static RegisterChatRoomResponse toRegisterChatRoomResponse(ChatRoom chatRoom){
 		return RegisterChatRoomResponse.of(chatRoom.getId());
+	}
+
+	public static <T> PageResponse<T> toPageResponse(Slice<T> page) {
+		return PageResponse.of(page);
+	}
+
+	public static ChatRoomSimpleResponse toChatRoomSimpleResponse(ChatRoom chatRoom, User receiver) {
+		return ChatRoomSimpleResponse.of(chatRoom.getId(), receiver.getNickname(), receiver.getProfileImageUrl());
 	}
 }
