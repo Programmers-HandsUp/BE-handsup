45f79dd81cdcf297ee99f3cd1cf681a5542f8fa4
hs12 <loveanna1228@gmail.com>
2024-03-05 00:01:35 +0900

    [feat] : chatRoomFixture 추가

---
+++ core/src/testFixtures/java/dev/handsup/fixture/ChatRoomFixture.java
@@ -0,0 +1,18 @@
+package dev.handsup.fixture;
+
+import static lombok.AccessLevel.*;
+
+import dev.handsup.chat.domain.ChatRoom;
+import dev.handsup.user.domain.User;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = PRIVATE)
+public class ChatRoomFixture {
+	public static ChatRoom chatRoom(Long auctionId, User seller, User buyer){
+		return ChatRoom.of(
+			auctionId,
+			seller,
+			buyer
+		);
+	}
+}
