701abbafb3a9bc08e7bbe42a8510b567c2275269
ParkJuHan94 <1004sunye@naver.com>
2024-03-04 16:22:46 +0900

    [feat] : BiddingResponse 필드 추가 - imgUrl, createAt

--- core/src/main/java/dev/handsup/bidding/dto/BiddingMapper.java
+++ core/src/main/java/dev/handsup/bidding/dto/BiddingMapper.java
@@ -24,7 +24,9 @@ public class BiddingMapper {
 			bidding.getBiddingPrice(),
 			bidding.getAuction().getId(),
 			bidding.getBidder().getId(),
-			bidding.getBidder().getNickname()
+			bidding.getBidder().getNickname(),
+			bidding.getAuction().getProduct().getImages().get(0).getImageUrl(),
+			bidding.getCreatedAt().toString()
 		);
 	}
 
