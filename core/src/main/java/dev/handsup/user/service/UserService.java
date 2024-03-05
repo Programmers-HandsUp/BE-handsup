278474ee1c36342f52a395403a87a4b02de2c2fd
ParkJuHan94 <1004sunye@naver.com>
2024-03-05 14:52:19 +0900

    [feat] : 유저 리뷰 라벨 조회 API 로직 추가

--- core/src/main/java/dev/handsup/user/service/UserService.java
+++ core/src/main/java/dev/handsup/user/service/UserService.java
@@ -1,17 +1,23 @@
 package dev.handsup.user.service;
 
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import dev.handsup.auth.domain.EncryptHelper;
+import dev.handsup.common.dto.CommonMapper;
+import dev.handsup.common.dto.PageResponse;
 import dev.handsup.common.exception.CommonErrorCode;
 import dev.handsup.common.exception.NotFoundException;
 import dev.handsup.common.exception.ValidationException;
 import dev.handsup.user.domain.User;
 import dev.handsup.user.dto.UserMapper;
 import dev.handsup.user.dto.request.JoinUserRequest;
+import dev.handsup.user.dto.response.UserReviewLabelResponse;
 import dev.handsup.user.exception.UserErrorCode;
 import dev.handsup.user.repository.UserRepository;
+import dev.handsup.user.repository.UserReviewLabelRepository;
 import lombok.RequiredArgsConstructor;
 
 @Service
@@ -20,6 +26,7 @@ public class UserService {
 
 	private final UserRepository userRepository;
 	private final EncryptHelper encryptHelper;
+	private final UserReviewLabelRepository userReviewLabelRepository;
 
 	private void validateDuplicateEmail(String email) {
 		if (userRepository.findByEmail(email).isPresent()) {
@@ -42,5 +49,13 @@ public class UserService {
 		validateDuplicateEmail(request.email());
 		User user = UserMapper.toUser(request, encryptHelper);
 		return userRepository.save(user).getId();
+	}
+
+	@Transactional(readOnly = true)
+	public PageResponse<UserReviewLabelResponse> getUserReviewLabels(Long userId, Pageable pageable) {
+		Slice<UserReviewLabelResponse> userReviewLabelResponsePage = userReviewLabelRepository
+			.findByUserIdOrderByIdAsc(userId, pageable)
+			.map(UserMapper::toUserReviewLabelResponse);
+		return CommonMapper.toPageResponse(userReviewLabelResponsePage);
 	}
 }
