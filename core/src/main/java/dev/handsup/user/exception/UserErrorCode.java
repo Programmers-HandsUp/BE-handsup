672ea2eb97ebe92bceb91eae2f98089eb57e079d
hs12 <loveanna1228@gmail.com>
2024-03-05 13:31:16 +0900

    [feat] : 유저 에러 코드 추가

--- core/src/main/java/dev/handsup/user/exception/UserErrorCode.java
+++ core/src/main/java/dev/handsup/user/exception/UserErrorCode.java
@@ -7,7 +7,7 @@ import lombok.RequiredArgsConstructor;
 @Getter
 @RequiredArgsConstructor
 public enum UserErrorCode implements ErrorCode {
-
+	NOT_FOUND_USER("해당 아이디의 유저가 존재하지 않습니다.", "U_000"),
 	NOT_FOUND_BY_EMAIL("EMAIL 에 해당하는 사용자가 존재하지 않습니다", "U_001"),
 	DUPLICATED_EMAIL("이미 존재하는 이메일입니다. 회원가입을 다시 진행해주세요.", "U_002"),
 	NON_VALIDATED_EMAIL("이메일 주소 양식을 확인해주세요.", "U_003");
