6376d31d91d94ce04e76d3abfd08b11cd204cbdc
hs12 <loveanna1228@gmail.com>
2024-03-06 10:43:21 +0900

    [feat] : 채팅방 조회 관련 errorCode 추가

--- core/src/main/java/dev/handsup/bidding/exception/BiddingErrorCode.java
+++ core/src/main/java/dev/handsup/bidding/exception/BiddingErrorCode.java
@@ -7,7 +7,7 @@ import lombok.RequiredArgsConstructor;
 @Getter
 @RequiredArgsConstructor
 public enum BiddingErrorCode implements ErrorCode {
-
+	NOT_FOUND_BIDDING("해당 아이디의 입찰이 존재하지 않습니다.", "B_000"),
 	BIDDING_PRICE_LESS_THAN_INIT_PRICE("입찰가는 최소 입찰가보다 높아야 합니다.", "B_001"),
 	BIDDING_PRICE_NOT_HIGH_ENOUGH("입찰가는 최고 입찰가보다 100원 이상 높아야 합니다.", "B_002");
 
--- core/src/main/java/dev/handsup/chat/exception/ChatErrorCode.java
+++ core/src/main/java/dev/handsup/chat/exception/ChatErrorCode.java
@@ -9,7 +9,10 @@ import lombok.RequiredArgsConstructor;
 public enum ChatErrorCode implements ErrorCode {
 
 	CHAT_ROOM_ALREADY_EXISTS("채팅방이 이미 존재합니다.", "CR_001"),
-	NOT_TRADING_AUCTION("거래 상태의 경매가 아닙니다.", "CR_002");
+	NOT_TRADING_AUCTION("거래 상태의 경매가 아닙니다.", "CR_002"),
+	CHAT_ROOM_ACCESS_DENIED("채팅방 조회 권한이 없습니다.", "CR_003"),
+	NOT_FOUND_CHAT_ROOM("해당 아이디의 채팅방이 존재하지 않습니다.", "CR_004");
+
 	private final String message;
 	private final String code;
 }
