931774ba45c2466b7b0b6fed9dba7427a1311386
hs12 <loveanna1228@gmail.com>
2024-03-06 11:33:05 +0900

    [feat] : 컨트롤러 메서드 추가

--- api/src/main/java/dev/handsup/controller/ChatRoomApiController.java
+++ api/src/main/java/dev/handsup/controller/ChatRoomApiController.java
@@ -3,12 +3,15 @@ package dev.handsup.controller;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import dev.handsup.auction.dto.response.ChatRoomExistenceResponse;
 import dev.handsup.auth.jwt.JwtAuthorization;
+import dev.handsup.chat.dto.response.ChatRoomDetailResponse;
 import dev.handsup.chat.dto.response.ChatRoomSimpleResponse;
 import dev.handsup.chat.dto.response.RegisterChatRoomResponse;
 import dev.handsup.chat.service.ChatRoomService;
@@ -44,10 +47,43 @@ public class ChatRoomApiController {
 	@ApiResponse(useReturnTypeSchema = true)
 	@GetMapping
 	public ResponseEntity<PageResponse<ChatRoomSimpleResponse>> getUserChatRooms(
-		@Parameter(hidden = true) @JwtAuthorization User user,
-		Pageable pageable
+		Pageable pageable,
+		@Parameter(hidden = true) @JwtAuthorization User user
 	) {
 		PageResponse<ChatRoomSimpleResponse> response = chatRoomService.getUserChatRooms(user, pageable);
+		return ResponseEntity.ok(response);
+	}
+
+	@Operation(summary = "채팅방 아이디로 채팅방 상세 조회", description = "채팅방 목록에서 채팅방 아이디로 채팅방을 상세 조회한다")
+	@ApiResponse(useReturnTypeSchema = true)
+	@GetMapping("/{chatRoomId}")
+	public ResponseEntity<ChatRoomDetailResponse> getChatRoomWithId(
+		@PathVariable("chatRoomId") Long chatRoomId,
+		@Parameter(hidden = true) @JwtAuthorization User user
+	) {
+		ChatRoomDetailResponse response = chatRoomService.getChatRoomWithId(chatRoomId, user);
+		return ResponseEntity.ok(response);
+	}
+
+	@Operation(summary = "입찰 아이디로 채팅방 상세 조회", description = "입찰 아이디로 경매 아이디, 입찰자를 얻어 채팅방을 상세 조회한다.")
+	@ApiResponse(useReturnTypeSchema = true)
+	@GetMapping("biddings/{biddingId}")
+	public ResponseEntity<ChatRoomDetailResponse> getChatRoomWithBiddingId(
+		@PathVariable("biddingId") Long biddingId,
+		@Parameter(hidden = true) @JwtAuthorization User user
+	) {
+		ChatRoomDetailResponse response = chatRoomService.getChatRoomWithBiddingId(biddingId, user);
+		return ResponseEntity.ok(response);
+	}
+
+	@Operation(summary = "입찰 아이디로 채팅방 상세 조회", description = "입찰 아이디로 경매 아이디, 입찰자를 얻어 채팅방을 상세 조회한다.")
+	@ApiResponse(useReturnTypeSchema = true)
+	@GetMapping("biddings/{biddingId}/existence")
+	public ResponseEntity<ChatRoomExistenceResponse> getChatRoomExistence(
+		@PathVariable("biddingId") Long biddingId,
+		@Parameter(hidden = true) @JwtAuthorization User user
+	) {
+		ChatRoomExistenceResponse response = chatRoomService.getChatRoomExistence(biddingId, user);
 		return ResponseEntity.ok(response);
 	}
 }
