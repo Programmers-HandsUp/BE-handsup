741c90bd92b7af62e5810488691c43a33fedbe78
hs12 <loveanna1228@gmail.com>
2024-03-06 10:43:01 +0900

    [feat] : 채팅방 조회 repository 함수 추가

--- core/src/main/java/dev/handsup/chat/repository/ChatRoomRepository.java
+++ core/src/main/java/dev/handsup/chat/repository/ChatRoomRepository.java
@@ -1,5 +1,7 @@
 package dev.handsup.chat.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -9,8 +11,10 @@ import dev.handsup.chat.domain.ChatRoom;
 import dev.handsup.user.domain.User;
 
 public interface ChatRoomRepository extends JpaRepository<ChatRoom, Long> {
+
 	@Query("SELECT cr FROM ChatRoom cr WHERE cr.seller = :user OR cr.bidder = :user")
 	Slice<ChatRoom> findChatRoomsByUser(User user, Pageable pageable);
 
+	Optional<ChatRoom> findChatRoomByAuctionIdAndBidder(Long auctionId, User bidder);
 	Boolean existsByAuctionIdAndBidder(Long auctionId, User bidder);
 }
