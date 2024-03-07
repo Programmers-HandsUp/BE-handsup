8431828273f5ce1fd518748cddc454f74b0f64dc
hs12 <loveanna1228@gmail.com>
2024-03-07 21:28:57 +0900

    [feat] : dto 및 mapper 추가

---
+++ core/src/main/java/dev/handsup/chat/dto/ChatMessageMapper.java
@@ -0,0 +1,21 @@
+package dev.handsup.chat.dto;
+
+import static lombok.AccessLevel.*;
+
+import dev.handsup.chat.domain.ChatMessage;
+import dev.handsup.chat.domain.ChatRoom;
+import dev.handsup.chat.dto.request.ChatMessageRequest;
+import dev.handsup.chat.dto.response.ChatMessageResponse;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = PRIVATE)
+public class ChatMessageMapper {
+
+	public static ChatMessage toChatMessage(ChatRoom chatRoom, ChatMessageRequest request) {
+		return ChatMessage.of(chatRoom, request.senderId(), request.content());
+	}
+
+	public static ChatMessageResponse toChatMessageResponse(ChatMessage chatMessage) {
+		return ChatMessageResponse.from(chatMessage);
+	}
+}
---
+++ core/src/main/java/dev/handsup/chat/dto/response/ChatMessageResponse.java
@@ -0,0 +1,19 @@
+package dev.handsup.chat.dto.response;
+
+import dev.handsup.chat.domain.ChatMessage;
+
+public record ChatMessageResponse(
+	Long chatRoomId,
+	Long senderId,
+	String content,
+	String createdAt
+) {
+	public static ChatMessageResponse from(ChatMessage chatMessage){
+		return new ChatMessageResponse(
+			chatMessage.getChatRoom().getId(),
+			chatMessage.getSenderId(),
+			chatMessage.getContent(),
+			chatMessage.getCreatedAt().toString()
+		);
+	}
+}
