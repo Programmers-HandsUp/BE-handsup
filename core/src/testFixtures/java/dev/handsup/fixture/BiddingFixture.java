e4628f433a3d46ac58499db82c11ebdc2fb88b40
hs12 <loveanna1228@gmail.com>
2024-03-09 18:21:13 +0900

    [feat] : biddingFixture 추가

--- core/src/testFixtures/java/dev/handsup/fixture/BiddingFixture.java
+++ core/src/testFixtures/java/dev/handsup/fixture/BiddingFixture.java
@@ -4,7 +4,7 @@ import static lombok.AccessLevel.*;
 
 import dev.handsup.auction.domain.Auction;
 import dev.handsup.bidding.domain.Bidding;
-import dev.handsup.bidding.domain.BiddingStatus;
+import dev.handsup.bidding.domain.TradingStatus;
 import dev.handsup.user.domain.User;
 import lombok.NoArgsConstructor;
 
@@ -16,7 +16,17 @@ public class BiddingFixture {
 			40000,
 			auction,
 			user,
-			BiddingStatus.WAITING
+			TradingStatus.WAITING
+		);
+	}
+
+	public static Bidding bidding(Auction auction, User user, TradingStatus status) {
+		return Bidding.of(
+			1L,
+			40000,
+			auction,
+			user,
+			status
 		);
 	}
 
