0edb746d97af609e1ef99b27349db526b0f7e170
hs12 <loveanna1228@gmail.com>
2024-03-09 16:53:41 +0900

    [feat] : 입찰 상태 enum 추가

---
+++ core/src/main/java/dev/handsup/bidding/domain/BiddingStatus.java
@@ -0,0 +1,16 @@
+package dev.handsup.bidding.domain;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@Getter
+public enum BiddingStatus {
+	WAITING("대기중"),
+	PREPARING("준비중"),
+	CHATTING("채팅중"),
+	CANCELED("취소됨"),
+	COMPLETED("완료됨");
+
+	private final String label;
+}
