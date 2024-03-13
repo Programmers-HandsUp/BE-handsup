bbde8f2b97ddcbfcc067e885057c7ab6b3956799
ParkJuHan94 <1004sunye@naver.com>
2024-03-13 20:05:51 +0900

    [rename] : request validation 메시지 수정

--- core/src/main/java/dev/handsup/user/dto/request/JoinUserRequest.java
+++ core/src/main/java/dev/handsup/user/dto/request/JoinUserRequest.java
@@ -11,25 +11,25 @@ import lombok.Builder;
 @Builder(access = PRIVATE)
 public record JoinUserRequest(
 
-	@NotBlank(message = "email 값이 공백입니다.")
+	@NotBlank(message = "email 은 필수입니다.")
 	@Pattern(regexp = "^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z]){0,19}"
 		+ "@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z]){0,19}[.][a-zA-Z]{2,3}$",
 		message = "이메일 주소 양식을 확인해주세요.")
 	String email,
 
-	@NotBlank(message = "password 값이 공백입니다.")
+	@NotBlank(message = "password 는 필수입니다.")
 	String password,
 
-	@NotBlank(message = "nickname 값이 공백입니다.")
+	@NotBlank(message = "nickname 은 필수입니다.")
 	String nickname,
 
-	@NotBlank(message = "주소의 시 값이 공백입니다.")
+	@NotBlank(message = "주소의 시는 필수입니다.")
 	String si,
 
-	@NotBlank(message = "주소의 구 값이 공백입니다.")
+	@NotBlank(message = "주소의 구는 필수입니다.")
 	String gu,
 
-	@NotBlank(message = "주소의 동 값이 공백입니다.")
+	@NotBlank(message = "주소의 동은 필수입니다.")
 	String dong,
 
 	@NotBlank(message = "profileImageUrl 은 필수입니다.")
