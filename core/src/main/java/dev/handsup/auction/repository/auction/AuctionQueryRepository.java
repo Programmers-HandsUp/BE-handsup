988a1d6973a01efc6ae09b5ff7ab822e13a10404
hs12 <loveanna1228@gmail.com>
2024-03-13 11:23:49 +0900

    [feat] : 카테고리로 경매 조회 동적 쿼리 작성

--- core/src/main/java/dev/handsup/auction/repository/auction/AuctionQueryRepository.java
+++ core/src/main/java/dev/handsup/auction/repository/auction/AuctionQueryRepository.java
@@ -1,13 +1,18 @@
 package dev.handsup.auction.repository.auction;
 
+import java.util.List;
+
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Slice;
 
 import dev.handsup.auction.domain.Auction;
+import dev.handsup.auction.domain.product.product_category.ProductCategory;
 import dev.handsup.auction.dto.request.AuctionSearchCondition;
 
 public interface AuctionQueryRepository {
 	Slice<Auction> searchAuctions(AuctionSearchCondition auctionSearchCondition, Pageable pageable);
 
 	Slice<Auction> sortAuctionByCriteria(String si, String gu, String dong, Pageable pageable);
+
+	Slice<Auction> findByProductCategories(List<ProductCategory> productCategories, Pageable pageable);
 }
