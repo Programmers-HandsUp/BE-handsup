878463473e9593cab84776bb2664a717674247f6
hs12 <loveanna1228@gmail.com>
2024-03-13 11:46:39 +0900

    [feat] : 유저 선호 카테고리 맞춤 추천 비즈니스 로직 작성

--- core/src/main/java/dev/handsup/auction/service/AuctionService.java
+++ core/src/main/java/dev/handsup/auction/service/AuctionService.java
@@ -2,12 +2,15 @@ package dev.handsup.auction.service;
 
 import static dev.handsup.auction.exception.AuctionErrorCode.*;
 
+import java.util.List;
+
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Slice;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import dev.handsup.auction.domain.Auction;
+import dev.handsup.auction.domain.product.product_category.PreferredProductCategory;
 import dev.handsup.auction.domain.product.product_category.ProductCategory;
 import dev.handsup.auction.dto.mapper.AuctionMapper;
 import dev.handsup.auction.dto.request.RegisterAuctionRequest;
@@ -16,6 +19,7 @@ import dev.handsup.auction.dto.response.RecommendAuctionResponse;
 import dev.handsup.auction.exception.AuctionErrorCode;
 import dev.handsup.auction.repository.auction.AuctionQueryRepository;
 import dev.handsup.auction.repository.auction.AuctionRepository;
+import dev.handsup.auction.repository.product.PreferredProductCategoryRepository;
 import dev.handsup.auction.repository.product.ProductCategoryRepository;
 import dev.handsup.common.dto.CommonMapper;
 import dev.handsup.common.dto.PageResponse;
@@ -29,6 +33,7 @@ public class AuctionService {
 
 	private final AuctionRepository auctionRepository;
 	private final ProductCategoryRepository productCategoryRepository;
+	private final PreferredProductCategoryRepository preferredProductCategoryRepository;
 	private final AuctionQueryRepository auctionQueryRepository;
 
 	public Auction getAuction(Long auctionId) {
@@ -54,6 +59,20 @@ public class AuctionService {
 		Slice<RecommendAuctionResponse> auctionResponsePage = auctionQueryRepository
 			.sortAuctionByCriteria(si, gu, dong, pageable)
 			.map(AuctionMapper::toRecommendAuctionResponse);
+		return CommonMapper.toPageResponse(auctionResponsePage);
+	}
+
+	@Transactional(readOnly = true)
+	public PageResponse<RecommendAuctionResponse> getUserRecommendAuctions(User user, Pageable pageable) {
+		List<ProductCategory> productCategories = preferredProductCategoryRepository.findByUser(user)
+			.stream()
+			.map(PreferredProductCategory::getProductCategory)
+			.toList();
+
+		Slice<RecommendAuctionResponse> auctionResponsePage = auctionQueryRepository
+			.findByProductCategories(productCategories, pageable)
+			.map(AuctionMapper::toRecommendAuctionResponse);
+
 		return CommonMapper.toPageResponse(auctionResponsePage);
 	}
 
