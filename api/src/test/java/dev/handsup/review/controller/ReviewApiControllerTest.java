59ff4e06bd71d3630319b878cdeca7c199db6b1a
ParkJuHan94 <1004sunye@naver.com>
2024-03-15 05:50:47 +0900

    [style] : 코드 리포맷팅

--- api/src/test/java/dev/handsup/review/controller/ReviewApiControllerTest.java
+++ api/src/test/java/dev/handsup/review/controller/ReviewApiControllerTest.java
@@ -8,7 +8,6 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 import java.util.List;
 
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
