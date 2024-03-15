f4323e420dfabf858daeafb7f64cb04e8a7e02e8
hs12 <loveanna1228@gmail.com>
2024-03-15 20:45:34 +0900

    [feat] : 채팅방 아이디로 채팅 메시지 조회 비즈니스 로직 작성

--- core/src/main/java/dev/handsup/chat/repository/ChatMessageRepository.java
+++ core/src/main/java/dev/handsup/chat/repository/ChatMessageRepository.java
@@ -1,8 +1,12 @@
 package dev.handsup.chat.repository;
 
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import dev.handsup.chat.domain.ChatMessage;
+import dev.handsup.chat.domain.ChatRoom;
 
 public interface ChatMessageRepository extends JpaRepository<ChatMessage, Long> {
+	Slice<ChatMessage> findByChatRoomOrderByCreatedAt(ChatRoom chatRoom, Pageable pageable);
 }
--- core/src/main/java/dev/handsup/chat/service/ChatRoomService.java
+++ core/src/main/java/dev/handsup/chat/service/ChatRoomService.java
@@ -13,11 +13,14 @@ import dev.handsup.bidding.domain.Bidding;
 import dev.handsup.bidding.exception.BiddingErrorCode;
 import dev.handsup.bidding.repository.BiddingRepository;
 import dev.handsup.chat.domain.ChatRoom;
+import dev.handsup.chat.dto.ChatMessageMapper;
 import dev.handsup.chat.dto.ChatRoomMapper;
+import dev.handsup.chat.dto.response.ChatMessageResponse;
 import dev.handsup.chat.dto.response.ChatRoomDetailResponse;
 import dev.handsup.chat.dto.response.ChatRoomSimpleResponse;
 import dev.handsup.chat.dto.response.RegisterChatRoomResponse;
 import dev.handsup.chat.exception.ChatRoomErrorCode;
+import dev.handsup.chat.repository.ChatMessageRepository;
 import dev.handsup.chat.repository.ChatRoomRepository;
 import dev.handsup.common.dto.CommonMapper;
 import dev.handsup.common.dto.PageResponse;
@@ -31,6 +34,7 @@ import lombok.RequiredArgsConstructor;
 public class ChatRoomService {
 
 	private final ChatRoomRepository chatRoomRepository;
+	private final ChatMessageRepository chatMessageRepository;
 	private final BiddingRepository biddingRepository;
 
 	@Transactional
@@ -83,6 +87,15 @@ public class ChatRoomService {
 		ChatRoom chatRoom = getChatRoomByCurrentBidding(bidding);
 
 		return ChatRoomMapper.toChatRoomDetailResponse(chatRoom, bidding, receiver);
+	}
+
+	@Transactional(readOnly = true)
+	public PageResponse<ChatMessageResponse> getChatRoomMessages(Long chatRoomId, Pageable pageable){
+		ChatRoom chatRoom = getChatRoomById(chatRoomId);
+		Slice<ChatMessageResponse> responsePage = chatMessageRepository
+			.findByChatRoomOrderByCreatedAt(chatRoom, pageable)
+			.map(ChatMessageMapper::toChatMessageResponse);
+		return CommonMapper.toPageResponse(responsePage);
 	}
 
 	private ChatRoom getChatRoomByCurrentBidding(Bidding currentBidding) {
