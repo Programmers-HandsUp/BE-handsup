23af779e5669031ee68a05bb09276237dfc2d2b5
hs12 <loveanna1228@gmail.com>
2024-03-16 14:40:05 +0900

    [feat] : Bidding 조회 시 fetch join으로 연관 객체 같이 조회

--- core/src/main/java/dev/handsup/bidding/repository/BiddingRepository.java
+++ core/src/main/java/dev/handsup/bidding/repository/BiddingRepository.java
@@ -6,11 +6,18 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import dev.handsup.bidding.domain.Bidding;
 import dev.handsup.bidding.domain.TradingStatus;
 
 public interface BiddingRepository extends JpaRepository<Bidding, Long> {
+
+	@Query("SELECT b FROM Bidding b "
+		+ "JOIN FETCH b.auction "
+		+ "JOIN FETCH b.bidder "
+		+ "WHERE b.id = :id")
+	Optional<Bidding> findBiddingWithAuctionAndBidder(@Param("id") Long id);
 
 	@Query("SELECT MAX(b.biddingPrice) FROM Bidding b WHERE b.auction.id = :auctionId")
 	Integer findMaxBiddingPriceByAuctionId(Long auctionId);
