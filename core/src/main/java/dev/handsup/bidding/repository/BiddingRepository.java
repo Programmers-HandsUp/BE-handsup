8a0fc5435dfec5200dcdc0ee775a0dc1c9c728ab
hs12 <loveanna1228@gmail.com>
2024-03-17 14:36:57 +0900

    [refactor] : 불필요한 로직 삭제

--- core/src/main/java/dev/handsup/bidding/repository/BiddingRepository.java
+++ core/src/main/java/dev/handsup/bidding/repository/BiddingRepository.java
@@ -1,21 +1,16 @@
 package dev.handsup.bidding.repository;
 
-import java.util.Optional;
-
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 import dev.handsup.bidding.domain.Bidding;
-import dev.handsup.bidding.domain.TradingStatus;
 
-public interface BiddingRepository extends JpaRepository<Bidding, Long> {
+public interface BiddingRepository extends JpaRepository<Bidding, Long>{
 
 	@Query("SELECT MAX(b.biddingPrice) FROM Bidding b WHERE b.auction.id = :auctionId")
 	Integer findMaxBiddingPriceByAuctionId(Long auctionId);
 
 	Slice<Bidding> findByAuctionIdOrderByBiddingPriceDesc(Long auctionId, Pageable pageable);
-
-	Optional<Bidding> findFirstByTradingStatus(TradingStatus tradingStatus);
 }
