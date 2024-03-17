b7b4b815543e8aa8dfb1b8c0270539cf425dfdd5
ParkJuHan94 <1004sunye@naver.com>
2024-03-18 05:00:21 +0900

    [feat] : NotificationType 거래 완료 알림 추가

--- core/src/main/java/dev/handsup/notification/domain/NotificationType.java
+++ core/src/main/java/dev/handsup/notification/domain/NotificationType.java
@@ -11,7 +11,8 @@ public enum NotificationType {
 	COMMENT("댓글 알림", "님이 회원님의 경매 물품에서 얘기하고 있어요."),
 	BOOKMARK("북마크 알림", "님이 회원님의 경매 물품을 관심있어 해요."),
 	PURCHASE_WINNING("구매 입찰의 낙찰 알림", "입찰하신 물품이 낙찰되었습니다."),
-	CANCELED_PURCHASE_WINNING("구매 입찰의 낙찰 취소 알림", "경매 낙찰이 취소되었습니다.");
+	CANCELED_PURCHASE_WINNING("구매 입찰의 거래 취소 알림", "거래가 취소되었습니다."),
+	COMPLETED_PURCHASE_WINNING("구매 입찰의 거래 완료 알림", "거래가 완료되었습니다.");
 
 	private final String title;
 	private final String content;
