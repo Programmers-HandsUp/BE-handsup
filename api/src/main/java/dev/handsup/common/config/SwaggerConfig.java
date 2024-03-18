095ce3bd5d253c5358c0762d53a3c14a84f05d46
ParkJuHan94 <1004sunye@naver.com>
2024-03-18 14:25:16 +0900

    [style] : 코드 리포매팅

--- api/src/main/java/dev/handsup/common/config/SwaggerConfig.java
+++ api/src/main/java/dev/handsup/common/config/SwaggerConfig.java
@@ -10,11 +10,11 @@ import org.springframework.context.annotation.Configuration;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.servers.Server;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
-import io.swagger.v3.oas.annotations.servers.Server;
 
 @OpenAPIDefinition(
 	info = @Info(
