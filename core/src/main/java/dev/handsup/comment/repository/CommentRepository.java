8eafbd9c77a4a8841ee9540ad46e50baf2d10887
hs12 <loveanna1228@gmail.com>
2024-03-19 22:39:58 +0900

    [feat] : comment repository 조회 로직 수정

--- core/src/main/java/dev/handsup/comment/repository/CommentRepository.java
+++ core/src/main/java/dev/handsup/comment/repository/CommentRepository.java
@@ -1,12 +1,13 @@
 package dev.handsup.comment.repository;
 
-import java.util.List;
-
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import dev.handsup.auction.domain.Auction;
 import dev.handsup.comment.domain.Comment;
 
 public interface CommentRepository extends JpaRepository<Comment, Long> {
-	List<Comment> findByAuction(Auction auction);
+
+	Slice<Comment> findByAuctionOrderByCreatedAtDesc(Auction auction, Pageable pageable);
 }
