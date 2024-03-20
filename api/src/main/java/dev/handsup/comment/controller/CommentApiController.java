af8feab37d2f0294de41b9e038edcde3a27dea1b
hs12 <loveanna1228@gmail.com>
2024-03-20 10:47:21 +0900

    [feat] : 댓글 조회 컨트롤러 메서드 작성

--- api/src/main/java/dev/handsup/comment/controller/CommentApiController.java
+++ api/src/main/java/dev/handsup/comment/controller/CommentApiController.java
@@ -1,16 +1,20 @@
 package dev.handsup.comment.controller;
 
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import dev.handsup.auth.annotation.NoAuth;
 import dev.handsup.auth.jwt.JwtAuthorization;
 import dev.handsup.comment.dto.request.RegisterCommentRequest;
 import dev.handsup.comment.dto.response.CommentResponse;
 import dev.handsup.comment.service.CommentService;
+import dev.handsup.common.dto.PageResponse;
 import dev.handsup.user.domain.User;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
@@ -39,6 +43,16 @@ public class CommentApiController {
 		return ResponseEntity.ok(response);
 	}
 
-	// TODO: 3/19/24 경매 아이디로 댓글 조회
+	@NoAuth
+	@Operation(summary = "댓글 조회 API", description = "한 경매에 대한 댓글을 모두 조회한다.")
+	@ApiResponse(useReturnTypeSchema = true)
+	@GetMapping("{auctionId}/comments")
+	public ResponseEntity<PageResponse<CommentResponse>> getAuctionComments(
+		@PathVariable("auctionId") Long auctionId,
+		Pageable pageable
+	) {
+		PageResponse<CommentResponse> response = commentService.getAuctionComments(auctionId, pageable);
+		return ResponseEntity.ok(response);
+	}
 
 }
